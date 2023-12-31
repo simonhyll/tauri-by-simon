688b61180263ed120f62236bb337b01bca401557
Simon Hyll <hyllsimon@gmail.com>
2023-12-31 16:09:20 +0100

    chore

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	base: '/tauri-by-simon',
 	integrations: [
 		starlight({
 			title: 'Tauri by Simon',
