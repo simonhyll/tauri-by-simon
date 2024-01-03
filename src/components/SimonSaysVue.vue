34059d2d34fa7ded2a03aef4e9f07772c9ef1359
Simon Hyll <hyllsimon@gmail.com>
2024-01-03 13:25:16 +0100

    fix for icon

--- src/components/SimonSaysVue.vue
+++ src/components/SimonSaysVue.vue
@@ -15,7 +15,7 @@ import { onMounted, onUnmounted, ref } from "vue";
 const props = defineProps({
   title: { type: String, default: "Psst!" },
 });
-const imageSrc = ref("/tauri-by-simon/public/icon.png");
+const imageSrc = ref("/tauri-by-simon/icon.png");
 </script>
 
 <style>
