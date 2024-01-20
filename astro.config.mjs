96231bc55af9f2ad9844fa2f44b84462e5c85325
Simon Hyll <hyllsimon@gmail.com>
2024-01-20 23:18:52 +0100

    feat: added sponsors page

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,6 +27,7 @@ export default defineConfig({
 		components: {
 			Footer: './src/components/Footer.astro',
 			Header: './src/components/Header.astro',
+			Sidebar: './src/components/Sidebar.astro',
 			SocialIcons: './src/components/SocialIcons.astro',
 		},
 		head: [{
